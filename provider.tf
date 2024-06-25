a30fbc1d1b2e05b42902e312bfae0da2a5854503
aashrith-snow <159412148+aashrith-snow@users.noreply.github.com>
2024-06-25 12:13:48 +0530

    Add files via upload

--- provider.tf
+++ provider.tf
@@ -1,5 +1,7 @@
-provider "aws" {
-  access_key      = "${var.access_key}"
-  secret_key      = "${var.secret_key}"
-  region          = "${var.region}"
+provider "azurerm" {
+  features {}
+  subscription_id = "${var.subscriptionId}"
+  client_id       = "${var.clientId}"
+  client_secret   = "${var.clientSecret}"
+  tenant_id       = "${var.tenantId}"
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,41 @@
-variable "access_key" {}
-variable "secret_key" {}
-variable "region" {}
-variable "subnet" {}
-variable "network" {}
-variable "instancetype" {}
-variable "ami" {}
-variable "keyname" {}
-variable "root_volume_size"{}
-variable "root_volume_type"{}
+variable clientId {}
+variable clientSecret {}
+variable region {}
+variable subscriptionId {}
+variable tenantId {}
+
+variable isNewResourceGroup {
+  type = bool
+}
+variable newResourceGroup {}
+variable existingResourceGroup {}
+variable vmName {}
+
+variable network {}
+variable subnet {}
+variable networkResourceGroup {}
+variable nic {}
+variable size {}
+variable adminUserName {}
+variable publicKey {}
+variable isPassword {
+  type = bool
+}
+variable password {}
+
+variable image_publisher {}
+variable image_offer {}
+variable image_sku {}
+variable image_version {}
+
+variable deleteOSDiskOnTerm {
+  type = bool
+}
+
 variable "disks" {
   type = list(object({
     volume_size  = number
     volume_type  = string
+    max_shares = number
   }))
-  default=[]
 }
