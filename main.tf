680abd50c5b316ca5e35ae6760bd7d3b22ef50ee
aashrith-snow <159412148+aashrith-snow@users.noreply.github.com>
2024-06-25 12:12:38 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,89 +1,40 @@
-provider "aws" {
-  region = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
+data "aws_ami" "image" {
+  most_recent      = true
+  owners           = ["amazon"]
 
-variable "lbName" {
-  type    = string
-  description = "name of aws lb"
-}
-
-
-variable "deploymentID" {
-  type    = string
-  description = "Deployment ID for the ELB"
-}
-
-resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "default_subnet_1" {
-  vpc_id                  = aws_vpc.default.id
-  cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-1a"
-}
-
-resource "aws_subnet" "default_subnet_2" {
-  vpc_id                  = aws_vpc.default.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1b"
-}
-
-resource "aws_internet_gateway" "example" {
-  vpc_id = aws_vpc.default.id
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.default.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.example.id
+  filter {
+    name   = "name"
+    values = ["${var.ami}"]
   }
-}
-resource "aws_security_group" "alb" {
-  name        = "alb-security-group"
-  vpc_id      = aws_vpc.default.id
-
-  # Define security group rules as needed
-}
-
-resource "aws_alb" "example" {
-  name            = "${var.lbName}"
-  security_groups = [aws_security_group.alb.id]
-  subnets         = [aws_subnet.default_subnet_1.id, aws_subnet.default_subnet_2.id]
-
-  lifecycle {
-    create_before_destroy = true
-  }
-    tags = {
-
-    deploymentID         = "${var.deploymentID}"
-
-    Environment = "Production"
-
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 }
 
-/*resource "aws_alb_listener" "example_listener" {
-  load_balancer_arn = aws_alb.example.arn
-  port              = 80
-  protocol          = "HTTP"
-  default_action {
-    type             = "fixed-response"
-    fixed_response {
-      content_type    = "text/plain"
-      message_body    = "OK"
-      status_code     = "200"
-    }
+resource "aws_instance" "basic_vm" {
+  ami           = data.aws_ami.image.id
+  instance_type = var.instancetype
+  key_name      = var.keyname
+  subnet_id	    = var.subnet
+  root_block_device {
+    volume_size           = var.root_volume_size
+    volume_type           = var.root_volume_type
+    iops                  = (var.root_volume_type == "io1" || var.root_volume_type == "io2") ? 100 : null
   }
-  tags = {
+}
+resource "aws_ebs_volume" "ebs_volumes" {
+  for_each = { for idx, disk in var.disks : idx => disk }
+  availability_zone = aws_instance.basic_vm.availability_zone
+  size = each.value.volume_size
+  type = each.value.volume_type
+  iops = (each.value.volume_type == "io1" || each.value.volume_type=="io2") ? 100 : null
+}
 
-    deploymentID         = "${var.deploymentID}"
-
-    Environment = "Production"
-
-  }
-}*/
+resource "aws_volume_attachment" "ebs_volume_attachments" {
+  for_each = aws_ebs_volume.ebs_volumes
+  device_name = "/dev/sd${substr("fghijklmnop", each.key, 1)}"
+  volume_id   = each.value.id
+  instance_id = aws_instance.basic_vm.id
+}
+locals {ebs_block_devices =  var.disks}
